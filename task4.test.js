22d2487a22b08cc5fedb7ca584defdc0c87d00e2
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-09 20:42:08 +0300

    finalise task4 jest test

--- task4.test.js
+++ task4.test.js
@@ -2,6 +2,6 @@ const capitalise = require('./task4');
 
 describe('task4', () => {
     test('Capitalise the first char of a string', () => {
-        expect(() => capitalise("hello")).toBe('Hello');
+        expect(capitalise('hello')).toBe('Hello');
     })
 })
